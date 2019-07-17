4dce11942e5eaa1113940b279f23a17f5662a123
Matthew Chen <mc.2green@gmail.com>
2019-07-17 15:32:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,9 +2,12 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var candyString = "foo"
  
  function addElementToBeginningOfArray(chcolateBars, candyString){
-   return [candyStriing, ...chocolateBars]
+   return [candyString, ...chocolateBars]
  }
  
  function destructivelyAddElementToEndOfArray(chcolateBars, candyString){
    return chocolateBars.upshift(candyString)
  }
+function accessElementInArray (){
+  
+}

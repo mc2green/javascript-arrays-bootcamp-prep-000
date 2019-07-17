80e9861d02cd846a347324a643ba0c5589c88ad0
Matthew Chen <mc.2green@gmail.com>
2019-07-17 15:37:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,6 +12,6 @@ function accessElementInArray (array, index){
   return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(){
-  
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
 }

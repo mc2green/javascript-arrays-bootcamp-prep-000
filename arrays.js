8bb832676af569b909e6c27fd8d59f42017448a1
Matthew Chen <mc.2green@gmail.com>
2019-07-17 15:50:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,43 +1,42 @@
-var chocolateBars=[‘snickers’,’hundred grand’, ‘kitkat’, ‘skittles’];
-var candyString=’foo’;
-
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+//var candyString = "foo"
+ 
 function addElementToBeginningOfArray(chocolateBars, candyString){
-return [candyString, …chocolateBars];
+  return [candyString, ...chocolateBars];
 }
-
+ 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
-chocolateBars.unshift(candyString);
-return chocolateBars;
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
 }
-
-function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
-chocolateBars.push(candyString);
-return chocolateBars;
-}
-
+ 
 function addElementToEndOfArray(chocolateBars, candyString){
-return […chocolateBars, candyString];
+  return [...chocolateBars, candyString];
 }
-
-function accessElementInArray(chocolateBars, index){
-return (chocolateBars[2]);
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
 }
-
+ 
+function accessElementInArray(array, index){
+  return array[index];
+}
+ 
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
-chocolateBars.shift(candyString);
-return chocolateBars;
+  chocolateBars.shift();
+  return chocolateBars;
 }
-
+ 
 function removeElementFromBeginningOfArray(chocolateBars){
-chocolateBars=chocolateBars.slice(1);
-return chocolateBars;
+  return chocolateBars.slice(1);
 }
+ 
 function destructivelyRemoveElementFromEndOfArray(chocolateBars){
-chocolateBars.pop()
-return chocolateBars;
+  chocolateBars.pop();
+  return chocolateBars;
 }
-
+ 
 function removeElementFromEndOfArray(chocolateBars){
-chocolateBars=chocolateBars.slice(0, chocolateBars.length-1);
-return chocolateBars;
+  return chocolateBars.slice(0, chocolateBars.length - 1);
 }

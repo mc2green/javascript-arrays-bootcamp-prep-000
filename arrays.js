5ec46836675e73bb6d9528560c54c9029c910075
Matthew Chen <mc.2green@gmail.com>
2019-07-17 15:48:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,22 +1,42 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
+//var candyString = "foo"
  
- function addElementToBeginningOfArray(chcolateBars, element){
-   return [element, ...chocolateBars]
- }
- 
- function destructivelyAddElementToEndOfArray(chcolateBars, element){
-   return chocolateBars.upshift(element)
- }
-function accessElementInArray (chocolateBars, index){
-  return chocolateBars[index]
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
 }
-
+ 
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+ 
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
+ 
+function accessElementInArray(array, index){
+  return array[index];
+}
+ 
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
-  return chocolateBars.shift()
+  chocolateBars.shift();
+  return chocolateBars;
 }
-function removeElementFromBeginningOfArray(array){
-   array = array.slice(0)
-   return array
-  
+ 
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+ 
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
+ 
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
 }
